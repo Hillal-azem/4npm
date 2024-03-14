e5a4110fde866041634254ec7673748dc5071460
hillal <286631@supinfo.com>
2024-03-14 15:06:12 +0100

    updated tasks to store il json file

--- tasks.js
+++ tasks.js
@@ -1,7 +1,26 @@
 import chalk from "chalk";
+import fs from "fs";
 
 function addTask() {
-  console.log(chalk.green("Adding a task"));
+  // add task to json file
+  fs.readFile("tasks.json", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    const tasks = JSON.parse(data);
+
+    // il faut récupérer les données de la requête depuis le body
+    tasks.push({ name: "New Task" });
+
+    fs.writeFile("tasks.json", tasks, (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log("Task added");
+    });
+  });
 }
 
 function removeTask() {
@@ -10,10 +29,27 @@ function removeTask() {
 
 function listAllTasks() {
   console.log(chalk.blue("Listing all tasks"));
+  fs.readFile("tasks.json", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    const tasks = JSON.parse(data);
+    // retroune les données dans la réponse de la requete
+  });
 }
 
 function findTask() {
   console.log(chalk.yellow("Finding a task"));
+  fs.readFile("tasks.json", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    const tasks = JSON.parse(data);
+    // filtrer les tasks grace à l'id reçu en paramètre de la requete
+    // retroune les données dans la réponse de la requete
+  });
 }
 
 function updateTask() {
